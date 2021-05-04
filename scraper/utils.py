ceccc1f74299e82b740e2d212edf9fb51c8e9593
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2021-05-04 14:34:32 +0200

    ajout des docstrings pour toutes les fonctions de utils.py

--- scraper/utils.py
+++ scraper/utils.py
@@ -10,7 +10,7 @@ from urllib.request import urlretrieve
 
 
 def scraping_category(url_category):
-  """Décrire toutes mes fonctions en expliquant sa fonction."""
+  """On vient récupérer dans le menu lattéral du site l'url d'une catégorie."""
   link_urls = []
   response = requests.get(url_category)
   if response.ok:
@@ -25,7 +25,7 @@ def scraping_category(url_category):
 
 
 def get_all_categories(url):
-  """Décrire toutes mes fonctions en expliquant sa fonction."""
+  """Ici on utilise également le menu mais pour prendre tous les urls de toutes les catégories."""
   link_urls = []
   response = requests.get(url)
   if response.ok:
@@ -40,7 +40,7 @@ def get_all_categories(url):
 
 
 def get_all_urls_book_from_one_category(url):
-    """Décrire toutes mes fonctions en expliquant sa fonction."""
+    """On vient prendre toutes les urls de tous les livres depuis une seule catégorie."""
     link_urls = []
     response = requests.get(url)
     if response.ok:
@@ -60,7 +60,7 @@ def get_all_urls_book_from_one_category(url):
 
 
 def get_all_url_book_in_categories(url_all_book_category):
-    """Décrire toutes mes fonctions en expliquant sa fonction."""
+    """On vient prendre toutes les urls de toutes les catégories."""
     link_urls = []
     response = requests.get(url_all_book_category)
     if response.ok:
@@ -86,7 +86,7 @@ def get_all_url_book_in_categories(url_all_book_category):
 
 
 def save_book_info_to_csv(book_info_list: list):
-    """Décrire toutes mes fonctions en expliquant sa fonction."""
+    """Cette partie s'occupe de sauvegarder les données récupérées, dans une boucle jusqu'à ce la fin de la liste."""
     first_book_info = book_info_list[0]
     category = first_book_info["category"].strip()
     with open(f"Scraping/{category}.csv", "w", encoding="utf-8-sig") as csvfile:
@@ -100,7 +100,8 @@ def save_book_info_to_csv(book_info_list: list):
 
 
 def scraping_book(url_book):
-    """Décrire toutes mes fonctions en expliquant sa fonction."""
+    """Fonction principale de l'application, ici on vient définir toutes les sous-fonctions,
+    pour que l'application récupères toutes les données d'un livre pour tout le site."""
     response = requests.get(url_book)  
     if response.ok:
         soup = BeautifulSoup(response.content, "html.parser")
@@ -129,7 +130,7 @@ def scraping_book(url_book):
 
 
 def product_description(soup):
-    """Décrire toutes mes fonctions en expliquant sa fonction."""
+    """Récupération de la description d'un livre."""
     description_element = soup.select_one(".sub-header ~ p")
     if description_element is not None:
       description = description_element.text
@@ -138,12 +139,13 @@ def product_description(soup):
     return description
 
 def product_category(soup):
+  """Récupération de la catégorie d'un livre."""
   category = soup.select(".breadcrumb li")[-2].text
   #print(category)
   return category.strip()
 
 def universal_product_code(soup):
-  """Décrire toutes mes fonctions en expliquant sa fonction."""
+  """Récupération du code produit d'un livre."""
   upc = soup.find_all("td")[-0].text
   table = soup.find("table")
   table_rows = table.find_all("tr")
@@ -151,13 +153,13 @@ def universal_product_code(soup):
   return upc
 
 def product_image_url(soup):
-  """Décrire toutes mes fonctions en expliquant sa fonction."""
+  """Récupération de l'url de l'image d'un livre."""
   image_url = soup.find("img")["src"]
   #print(image_url)
   return image_url
 
 def product_number_available(soup):
-  """Décrire toutes mes fonctions en expliquant sa fonction."""
+  """Récupération de la quantité disponible, du nombre, d'un livre."""
   number = soup.find_all("td")[5].text
   table = soup.find("table")
   table_rows = table.find_all("tr")
@@ -165,7 +167,7 @@ def product_number_available(soup):
   return number
 
 def product_price_including(soup):
-  """Décrire toutes mes fonctions en expliquant sa fonction."""
+  """Récupération du prix d'un livre avec taxes."""
   including = soup.find_all("td")[3].text
   table = soup.find("table")
   table_rows = table.find_all("tr")
@@ -173,7 +175,7 @@ def product_price_including(soup):
   return including
   
 def product_price_excluding(soup):
-  """Décrire toutes mes fonctions en expliquant sa fonction."""
+  """Récupération du prix d'un livre sans taxes."""
   excluding = soup.find_all("td")[2].text
   table = soup.find("table")
   table_rows = table.find_all("tr")
@@ -181,7 +183,7 @@ def product_price_excluding(soup):
   return excluding
 
 def product_review_rating(soup):
-  """Décrire toutes mes fonctions en expliquant sa fonction."""
+  """Récupération de la note sur cinq étoiles d'un livre."""
   review_rating = soup.find("p", "star-rating")["class"][1]
   #print(review_rating)
   return review_rating
